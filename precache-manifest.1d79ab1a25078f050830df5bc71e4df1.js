5f0e1d0b548b1567930e4a7f007bfa924a373bb8
Ninad Shenoy <ninad.shenoy@cerner.com>
2020-05-17 23:01:11 +0530

    Updates

---
+++ precache-manifest.1d79ab1a25078f050830df5bc71e4df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6865cf86d72f7afabc54bdaecf2e32dd",
+    "url": "/foxtrot/index.html"
+  },
+  {
+    "revision": "1f85d2ee71e0d3c5325e",
+    "url": "/foxtrot/static/css/main.070ebf8d.chunk.css"
+  },
+  {
+    "revision": "c5cf289815da86215f05",
+    "url": "/foxtrot/static/js/2.d058a947.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/foxtrot/static/js/2.d058a947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f85d2ee71e0d3c5325e",
+    "url": "/foxtrot/static/js/main.0b8fac3a.chunk.js"
+  },
+  {
+    "revision": "629ea0a05499816d7017",
+    "url": "/foxtrot/static/js/runtime-main.a1b89535.js"
+  }
+]);
