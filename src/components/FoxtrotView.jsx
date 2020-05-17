f89a5d34aab16d3d347085ec8412b7dde5b36db0
Ninad Shenoy <ninad.shenoy@cerner.com>
2020-05-17 22:46:46 +0530

    Initial commit

---
+++ src/components/FoxtrotView.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FoxtrotView extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default FoxtrotView;
